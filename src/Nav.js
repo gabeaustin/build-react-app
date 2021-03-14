436e2208abb0ed17709e0662049f2ec516822f74
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-13 20:14:37 -0600

    added link to navbar to include link to

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
+
+    const navStyle = {
+        color: "white",
+        textDecoration: "none"
+    };
+
     return (
-        <nav className = "nav-links">
-            <h3>Logo</h3>
+        <nav>
+            <Link style={ navStyle } to="/">
+                <h3>Logo</h3>
+            </Link>
             <ul>
-                <li>About</li>
-                <li>Shop</li>
+                <Link style={ navStyle } to="/About">
+                    <li className="nav-links">About</li>
+                </Link>
+                <Link style={ navStyle } to="/Shop">
+                    <li className="nav-links">Shop</li>
+                </Link>
             </ul>
         </nav>
     );
